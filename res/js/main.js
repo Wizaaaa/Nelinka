3bf8f32a6b8ef4ddaa1b2aac671eb73f93e2753f
Wizaaaa <jakubsalaky@gmail.com>
2025-01-29 14:37:56 +0100

    futures

--- res/js/main.js
+++ res/js/main.js
@@ -1,9 +1,30 @@
 const yes = document.getElementById('yes');
 const no = document.getElementById('no');
+const nadpis = document.getElementById('nadpis');
+
+let idk = [
+    "Proč 😭",
+    "😭😭😭😭😭😭😭😭😭",
+    "To ranilo mé srdíčko",
+    "💔💔💔",
+    "Au",
+    "Budu bečet",
+    "💀",
+    "Nemám slov"
+];
 
 no.addEventListener('mouseover', () => {
     const x = Math.random() * 1000;
-    const y = Math.random() * 1000;
+    const y = Math.random() * 900;
     no.style.left = x + 'px';
     no.style.top = y + 'px';
+    nadpis.innerHTML = idk[Math.floor(Math.random() * idk.length)];
 });
+
+yes.onclick = () => {
+    nadpis.innerHTML = 'JUPIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII';
+}
+
+no.onclick = () => {
+    nadpis.innerHTML = 'Reálně nemám tušení jak se ti to povedlo, protože by to mělo být nereálný, ale gj :D';
+}
